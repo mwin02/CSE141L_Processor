ee382a5ac51691f72cb769cc1fa2b94434e8dec7
David Hong <david.hong199@gmail.com>
2023-11-18 00:14:59 -0800

    reorganized files

--- CSE 141L Writeup/class_demo_proc/PC.sv
+++ CSE 141L Writeup/class_demo_proc/PC.sv
@@ -4,22 +4,20 @@
 module PC #(parameter D=12)(
   input reset,					// synchronous reset
         clk,
-		  branch_en,
-		  reljump_en,             // rel. jump enable
+		reljump_en,             // rel. jump enable
         absjump_en,				// abs. jump enable
   input       [D-1:0] target,	// how far/where to jump
   output logic[D-1:0] prog_ctr
 );
 
   always_ff @(posedge clk)
-   if(reset)
+    if(reset)
 	  prog_ctr <= '0;
-	else if(branch_en) begin
-	  if(reljump_en)
-	    prog_ctr <= prog_ctr + target;
-     else if(absjump_en)
-	    prog_ctr <= target;
-	end else
+	else if(reljump_en)
+	  prog_ctr <= prog_ctr + target;
+    else if(absjump_en)
+	  prog_ctr <= target;
+	else
 	  prog_ctr <= prog_ctr + 'b1;
 
 endmodule
