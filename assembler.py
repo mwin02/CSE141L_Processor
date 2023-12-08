2ac9a47b1fc16b38b0166f87ac5aa45a36af9011
Myo Zaw Win <62888717+mwin02@users.noreply.github.com>
2023-12-08 09:47:33 -0800

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -152,5 +152,5 @@ if __name__ == "__main__":
     assembler = Assembler()
     assembler.assemble('program1_assembly.txt', 'program1_machine_code.txt')
     assembler.assemble('program2_assembly.txt', 'program2_machine_code.txt')
-    assembler.assemble('/program3_assembly.txt', 'program3_machine_code.txt')
+    assembler.assemble('program3_assembly.txt', 'program3_machine_code.txt')
     assembler.assemble('test.txt','test_machine.txt')
